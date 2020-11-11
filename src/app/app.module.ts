eb0c6618cd2ba9d5d5474ab0b2168e80f97f245c
krystian.krasowski <krystiankrasowski@gmail.com>
2020-11-11 13:36:03 +0100

    changed navigation from sidenav to toolbar with buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CategoriesComponent } from './categories/categories.component';
@@ -21,8 +21,8 @@ import { CategoriesComponent } from './categories/categories.component';
     BrowserAnimationsModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
